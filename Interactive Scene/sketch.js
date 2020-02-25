9df5c67d98c3d4929361331be1145202234f20df
Ray Dai <58191367+IdkWhatsAGoodName@users.noreply.github.com>
2020-02-25 15:14:14 -0600

    Bouncing Physics (1/2)
    Bouncing physics put in for bottom and left border, improvement needed

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -5,25 +5,49 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let bouncy
-let ball = {
-  x_cord: 0,
-  y_cord: 0,
-  x_velocity: 0,
-  y_velocity: 0,
-};
+let bouncy;
+let gravity = 0.98;
+
+let x_cord = 0;
+let y_cord = 0;
+let x_velocity = 0;
+let y_velocity = 0;
+
+let left_border;
+let right_border;
+let top_border;
+let bottom_border;
 
 function preload(){
-  bouncy = loadImage("assets/bouncy_ball.png")
+  bouncy = loadImage("assets/bouncy_ball.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ball.x_cord = windowWidth/2 - bouncy.width/2;
-  ball.y_cord = windowHeight/2 - bouncy.height/2;
+  x_cord = windowWidth/2 - bouncy.width/2;
+  y_cord = windowHeight/2 - bouncy.height/2;
 }
 
 function draw() {
   background(220);
-  image(bouncy, ball.x_cord, ball.y_cord);
+  moveBall();
+  image(bouncy, x_cord, y_cord);
+}
+
+function moveBall() {
+  y_velocity += gravity;
+  if(x_cord + x_velocity > bouncy.width/2 && x_cord + x_velocity < windowWidth - bouncy.width/2 && y_cord + y_velocity > bouncy.height && y_cord + y_velocity < windowHeight - bouncy.height){
+    x_cord += x_velocity;
+    y_cord += y_velocity;
+  }
+  else if(x_cord + x_velocity < bouncy.width/2 && y_cord + y_velocity > bouncy.height/2 && y_cord + y_velocity < windowHeight - bouncy.height){
+    x_cord = bouncy.width/2;
+    y_cord += y_velocity;
+    x_velocity = -x_velocity;
+  }
+  else if(x_cord + x_velocity > bouncy.width/2 && x_cord + x_velocity < windowWidth - bouncy.width/2 && y_cord + y_velocity > windowHeight - bouncy.height/2){
+    x_cord += x_velocity;
+    y_cord =  windowHeight - bouncy.height/2;
+    y_velocity = -y_velocity;
+  }
 }
