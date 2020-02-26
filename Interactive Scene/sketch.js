2077abe7637d3d6820b1a9ef1aa5095d42504e74
Ray Dai <58191367+IdkWhatsAGoodName@users.noreply.github.com>
2020-02-26 15:22:25 -0600

    Code Clean Up
    Cleaned up code by renaming variables, physics stopped working again, fix needed

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -8,15 +8,16 @@
 let bouncy;
 let gravity = 0.98;
 
-let x_cord = 0;
-let y_cord = 0;
-let x_velocity = 0;
-let y_velocity = 0;
+let xCord = 0;
+let yCord = 0;
+// velocity is split into horizontal and vertical vectors
+let xVelocity = 0;
+let yVelocity = 0;
 
-let left_border;
-let right_border;
-let top_border;
-let bottom_border;
+let leftBorder;
+let rightBorder;
+let topBorder;
+let bottomBorder;
 
 function preload(){
   // load the ball sprite
@@ -27,36 +28,49 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   
   // define the borders of the ball's movement
-  left_border = 0;
-  right_border = windowWidth - bouncy.width;
-  top_border = 0;
-  bottom_border = windowHeight - bouncy.height;
+  leftBorder = 0;
+  rightBorder = windowWidth - bouncy.width;
+  topBorder = 0;
+  bottomBorder = windowHeight - bouncy.height;
   
   // place the ball at the center
-  x_cord = windowWidth/2 - bouncy.width/2;
-  y_cord = windowHeight/2 - bouncy.height/2;
+  xCord = windowWidth/2 - leftBorder;
+  yCord = windowHeight/2 - bouncy.height/2;
 }
 
+// move ball
 function draw() {
   background(220);
   moveBall();
-  image(bouncy, x_cord, y_cord);
+  image(bouncy, xCord, yCord);
 }
 
 function moveBall() {
-  y_velocity += gravity;
-  if(x_cord + x_velocity > bouncy.width/2 && x_cord + x_velocity < windowWidth - bouncy.width/2 && y_cord + y_velocity > bouncy.height && y_cord + y_velocity < windowHeight - bouncy.height/2){
-    x_cord += x_velocity;
-    y_cord += y_velocity;
+
+  // gravity accelerates ball downwards 
+  yVelocity += gravity;
+  
+  // map out the location of the ball after moving
+  let nextXCord = xCord + xVelocity;
+  let nextYCord = yCord + yVelocity;
+  
+  // if ball isn't about to run into a border, move ball in straight line
+  if(nextXCord > leftBorder && nextXCord < rightBorder && nextYCord > bottomBorder && nextYCord < bottomBorder){
+    xCord += xVelocity;
+    yCord += yVelocity;
   }
-  else if(x_cord + x_velocity < bouncy.width/2 && y_cord + y_velocity > bouncy.height/2 && y_cord + y_velocity < windowHeight - bouncy.height){
-    x_cord = bouncy.width/2;
-    y_cord += y_velocity;
-    x_velocity = -x_velocity;
+  
+  // ball bouncing off left border
+  else if(nextXCord <= leftBorder && nextYCord > topBorder && nextYCord < bottomBorder){
+    xCord = leftBorder;
+    yCord += yVelocity;
+    xVelocity = -xVelocity;
   }
-  else if(x_cord + x_velocity > bouncy.width/2 && x_cord + x_velocity < windowWidth - bouncy.width/2 && y_cord + y_velocity > windowHeight - bouncy.height/2){
-    x_cord += x_velocity;
-    y_cord =  windowHeight - bouncy.height/2;
-    y_velocity = -y_velocity;
+
+  // ball bouncing off bottom border
+  else if(nextXCord > leftBorder && nextXCord < rightBorder && nextYCord > bottomBorder){
+    xCord += xVelocity;
+    yCord =  bottomBorder;
+    yVelocity = -yVelocity;
   }
 }
