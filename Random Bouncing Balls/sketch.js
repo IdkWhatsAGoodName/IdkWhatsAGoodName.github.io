245d8afb80f1e34183af0ba79a867b67d25a5203
Ray Dai <58191367+IdkWhatsAGoodName@users.noreply.github.com>
2020-04-06 17:17:30 -0600

    Pausing Random Balls Project
    I did some things on this project, and it's nowhere from complete, but I'll leave it for now to work on other things.

--- Random Bouncing Balls/sketch.js
+++ Random Bouncing Balls/sketch.js
@@ -9,18 +9,23 @@ let GRAVITATIONAL_CONSTANT = 0.98;
 
 let ballArray = [];
 
+let gameRunning = true;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  window.setInterval(addBall,10);
+  window.setInterval(addBall,1000);
 }
 
 function draw() {
   background(220);
-  displayBall();
-  moveBall();
-  collisionDetection();
+  if (gameRunning){
+    displayBall();
+    moveBall();
+    collisionDetection();
+  }
 }
 
+// drawing all balls
 function displayBall(){
   for(let i = 0; i < ballArray.length; i++){
     fill(ballArray[i].color);
@@ -28,38 +33,43 @@ function displayBall(){
   }
 }
 
+// creating balls
 function addBall(){
   noStroke();
   let thisBall = {
     x: random(width),
     y: random(height),
+    dx: random(-10, 10),
+    dy: random(-10, 10),
     radius: random(25,50),
     color: color(random(255), random(255), random(255), random(255)),
   };
   ballArray.push(thisBall);
 }
 
+// moving each ball according to a random acceleration
 function moveBall() {
-  for(let i=0; i<ballArray.length; i++){
-    let dx = random(-10, 10);
-    let dy = random(-10, 10);
-    ballArray[i].x += dx;
-    ballArray[i].y += dy;
+  for (let i = 0; i < ballArray.length; i++){
+    accelerationX = random(-10, 10);
+    accelerationY = random(-10, 10);
+    ballArray[i].dx += accelerationX;
+    ballArray[i].dy += accelerationY;
+    ballArray[i].x += ballArray[i].dx;
+    ballArray[i].y += ballArray[i].dy;
   }
 }
 
 function collisionDetection(){
   for(let i=0; i<ballArray.length; i++){
+    // mouse collision detection
     let mouseDistance = dist(mouseX, mouseY, ballArray[i].x, ballArray[i].y);
     if(mouseDistance <= ballArray[i].radius){
-      ballArray.splice(i,1);
+      console.log("You died.");
+      gameRunning = false;
     }
   }
 }
 
-// function mousePressed(){
-//   collisionDetection();
-// }
 
 function windowResized(){
   setup();
